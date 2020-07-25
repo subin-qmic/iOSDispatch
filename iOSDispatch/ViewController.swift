4a04eda05bb7fe328055002309f10eafe1b5e088
Info qmic <infoqmic@Sajjeels-MacBook-Pro-2.local>
2020-07-25 11:29:56 +0300

    Initial commit

---
+++ iOSDispatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSDispatch
+//
+//  Created by Info qmic on 7/25/20.
+//  Copyright © 2020 Info qmic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
